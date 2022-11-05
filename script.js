8b8b3fcc3dfe7427f9913c839fc0517f649ccb0a
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2022-11-06 01:10:42 +0530

    new Project of Currency Conversion

---
+++ script.js
@@ -0,0 +1,14 @@
+// Api Key = 6Hv8YF2p10IKrYMB87WmJMaZdJoNlUf2
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", "6Hv8YF2p10IKrYMB87WmJMaZdJoNlUf2");
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+}
+
+fetch(`https://api.apilayer.com/currency_data/list`, requestOptions)
+.then(Response => Response.json())
+.then(result => console.log(result));
